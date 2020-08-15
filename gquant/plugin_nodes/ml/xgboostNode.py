bb23da409248c2b79870deaedcba11cfa234aa78
Yi Dong <doyend@gmail.com>
2020-08-15 12:17:46 -0400

    fixed the colum calculation for inference

--- gquant/plugin_nodes/ml/xgboostNode.py
+++ gquant/plugin_nodes/ml/xgboostNode.py
@@ -7,6 +7,7 @@ from gquant.dataframe_flow._port_type_node import _PortTypesMixin
 from gquant.dataframe_flow.portsSpecSchema import (ConfSchema,
                                                    PortsSpecSchema, NodePorts)
 from xgboost import Booster
+import copy
 
 
 __all__ = ['TrainXGBoostNode', 'InferXGBoostNode']
@@ -323,25 +324,46 @@ class InferXGBoostNode(Node, _PortTypesMixin):
 
     def columns_setup(self):
         input_columns = self.get_input_columns()
-        if self.INPUT_PORT_NAME in input_columns:
+        self.required = {self.INPUT_PORT_NAME: {},
+                         self.INPUT_PORT_MODEL_NAME: {}}
+        predict = self.conf.get('prediction', 'predict')
+        output_cols = {
+            self.OUTPUT_PORT_NAME: {predict: None}
+        }
+        if (self.INPUT_PORT_NAME in input_columns
+                and self.INPUT_PORT_MODEL_NAME in input_columns):
             col_from_inport = input_columns[self.INPUT_PORT_NAME]
-            if self.INPUT_PORT_MODEL_NAME in input_columns:
-                required_cols = input_columns[
-                    self.INPUT_PORT_MODEL_NAME]['train']
-                predict = self.conf.get('prediction', 'predict')
-                col_from_inport[predict] = None # the type is not determined 
-                self.required = {self.INPUT_PORT_NAME: required_cols,
-                                 self.INPUT_PORT_MODEL_NAME: {}}
+            required_cols = input_columns[self.INPUT_PORT_MODEL_NAME]['train']
+            predict = self.conf.get('prediction', 'predict')
+            col_from_inport[predict] = None  # the type is not determined
+            self.required = {self.INPUT_PORT_NAME: required_cols,
+                             self.INPUT_PORT_MODEL_NAME: {}}
             output_cols = {
                 self.OUTPUT_PORT_NAME: col_from_inport,
             }
             return output_cols
-        else:
-            col_from_inport = {}
+        elif (self.INPUT_PORT_NAME not in input_columns and
+              self.INPUT_PORT_MODEL_NAME in input_columns):
+            required_cols = input_columns[self.INPUT_PORT_MODEL_NAME]['train']
+            predict = self.conf.get('prediction', 'predict')
+            col_from_inport = copy.copy(required_cols)
+            col_from_inport[predict] = None  # the type is not determined
+            self.required = {self.INPUT_PORT_NAME: required_cols,
+                             self.INPUT_PORT_MODEL_NAME: {}}
             output_cols = {
-                self.OUTPUT_PORT_NAME: col_from_inport,
+                self.OUTPUT_PORT_NAME: col_from_inport
             }
             return output_cols
+        elif (self.INPUT_PORT_NAME in input_columns and
+              self.INPUT_PORT_MODEL_NAME not in input_columns):
+            col_from_inport = input_columns[self.INPUT_PORT_NAME]
+            predict = self.conf.get('prediction', 'predict')
+            col_from_inport[predict] = None  # the type is not determined
+            output_cols = {
+                self.OUTPUT_PORT_NAME: col_from_inport
+            }
+            return output_cols
+        return output_cols
 
     def ports_setup(self):
         types = [cudf.DataFrame,
