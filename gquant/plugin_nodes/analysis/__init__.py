9b47d85c54526c3641cf0074a7c6cbf002e84dba
Yi Dong <doyend@gmail.com>
2020-08-15 18:42:03 -0400

    checking the model export node

--- gquant/plugin_nodes/analysis/__init__.py
+++ gquant/plugin_nodes/analysis/__init__.py
@@ -5,7 +5,8 @@ from .barPlotNode import BarPlotNode
 from .linePlotNode import LinePlotNode
 from .rocCurveNode import RocCurveNode
 from .importanceCurve import ImportanceCurveNode
+from .exportXGBoostNode import XGBoostExportNode
 
 __all__ = ["OutCsvNode", "SharpeRatioNode", "CumReturnNode",
            "BarPlotNode", "LinePlotNode", "RocCurveNode",
-           "ImportanceCurveNode"]
+           "ImportanceCurveNode", "XGBoostExportNode"]
